1ae00a50b2fe38f3aa659367c243421743d6052d
Yakov <chibizov96@gmail.com>
2020-11-08 20:09:06 +0300

    Lesson 4 add
    All done

---
+++ Lesson 4-6.py
@@ -0,0 +1,21 @@
+'''Реализовать два небольших скрипта:
+а) бесконечный итератор, генерирующий целые числа, начиная с указанного,
+б) бесконечный итератор, повторяющий элементы некоторого списка, определенного заранее.
+Подсказка: использовать функцию count() и cycle() модуля itertools.'''
+from itertools import count
+from itertools import cycle
+
+def my_count_func(start_number, stop_number):
+    for el in count(start_number):
+        if el > stop_number:
+            break
+        else:
+            print(el)
+def my_cycle_func(my_list, iteration):
+    i = 0
+    iter = cycle(my_list)
+    while i < iteration:
+        print(next(iter))
+        i+=1
+my_count_func(start_number = int(input("enter start number: ")), stop_number = int(input("enter stop number: ")))
+my_cycle_func(my_list = [1, 2, 3, 4], iteration = int(input("enter iteration: ")))
